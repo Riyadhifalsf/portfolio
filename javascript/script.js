f39101d352d5738f479f9f0123b8ccca7442c478
Rydhflsf <gbzleopard@gmail.com>
2021-02-20 15:49:22 -0800

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+//Loader
+$(document).ready(function() {
+    $(".bg-loader").hide();
+})
+
+//Efek Ngetik
+const txtElement = ['Programmer', 'Web Developer', 'Blogger'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 1000);
+
+})();
